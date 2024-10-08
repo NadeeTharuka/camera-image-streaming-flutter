96a7ed3896f8c48768727a8c56b7f1a352cdaa85
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 15:11:02 +0530

    Update scan_controller.dart

--- camera/lib/scan_controller.dart
+++ camera/lib/scan_controller.dart
@@ -1,12 +1,36 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+
 import 'package:get/state_manager.dart';
+import 'package:camera/camera.dart';
+
 
 class ScanController extends GetxController {
   final RxBool _isInitialized = RxBool(false);
+  late CameraController -cameraController;
+  late List<CameraDescription> _cameras;
 
   bool get isInitialized => _isInitialized.value;
 
-  Future<void> initCamera() {}
+  Future<void> initCamera() async{
+    _camera = await availableCameras();
+    _cameraController = CameraController(_cameras[0], ResolutionPreset.max);
+    _cameraController.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
 
   @override
   void onInit() {
