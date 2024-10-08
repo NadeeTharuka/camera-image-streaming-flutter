a1d8a38ba4c7b5d942b3d48dc44822174e6ba656
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 15:24:02 +0530

    Update camera_screen.dart

--- camera/lib/camera/camera_screen.dart
+++ camera/lib/camera/camera_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'camera_app/scan_controller.dart';
-
+import 'package:camera_app/scan_controller.dart';
+import 'package:camera/camera.dart';
 
 class CameraScreen extends StatelessWidget {
   const CameraScreen({Key? key}) : super(key: key);
