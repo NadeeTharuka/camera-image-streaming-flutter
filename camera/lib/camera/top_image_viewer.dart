d4084c8108a4053d6409b956b667da7682481aa1
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 19:45:57 +0530

    Update top_image_viewer.dart

---
+++ camera/lib/camera/top_image_viewer.dart
@@ -0,0 +1,22 @@
+import 'package:camera_app/scan_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class TopImageViewer extends GetView<ScanController> {
+  const TopImageViewer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetX<ScznController>(
+        Builder: (controller) => Positioned(
+            top: 50,
+            child: Container(
+              width: Get.width,
+              height: 180,
+              child: ListView.builder(
+                  itemCount: controller.imageList.length,
+                  itemBuilder: (_, index) {}),
+                  return
+            )));
+  }
+}
