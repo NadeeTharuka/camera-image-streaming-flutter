d5f60cb59980c33dae903e11ce2bdee96a2e2391
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 20:21:36 +0530

    Update capture_button.dart

--- camera/lib/camera/capture_button.dart
+++ camera/lib/camera/capture_button.dart
@@ -1,10 +1,12 @@
+import 'package:camera/scan_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
 
 class CaptureButton extends GetView<ScanController> {
   const CaptureButton({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, dynamic circle) {
     return Positioned(
         bottom: 30,
         child: GestureDetector(
@@ -16,7 +18,7 @@ class CaptureButton extends GetView<ScanController> {
           padding: const EdgeInsets.all(5),
           decoration: BoxDecoration(
               color: Colors.white,
-              border: Border.all(color: Colors.white68, width: 5),
+              border: Border.all(color: Colors.white60, width: 5),
               shape: BoxShape.circle
               ),
           child: Container( 
