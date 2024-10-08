2950322344ad7e4d5de11e86fd8319787d17417b
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 20:21:44 +0530

    Update global_bindings.dart

--- camera/lib/global_bindings.dart
+++ camera/lib/global_bindings.dart
@@ -1,5 +1,7 @@
+import 'package:camera/scan_controller.dart';
 import 'package:camera_app/scan_controller.dart';
 import 'package:get/instance_manager.dart';
+import 'package:get/get.dart';
 
 class GlobalBindings extends Bindings {
   @override
