6c76f1cdebc2e7c059bd4dd68dc5edd7ee50af37
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 19:28:37 +0530

    Update scan_controller.dart

--- camera/lib/scan_controller.dart
+++ camera/lib/scan_controller.dart
@@ -1,24 +1,28 @@
 
+import 'dart:typed_data';
+
 import 'package:get/state_manager.dart';
 import 'package:camera/camera.dart';
+import 'package:image/image.dart' as img;
 
 
 class ScanController extends GetxController {
   final RxBool _isInitialized = RxBool(false);
   late CameraController -cameraController;
   late List<CameraDescription> _cameras;
+  late CameraImage _cameraImage;
 
   bool get isInitialized => _isInitialized.value;
   CameraController get cameraController => _cameraController;
 
   Future<void> initCamera() async{
     _camera = await availableCameras();
-    _cameraController = CameraController(_cameras[0], ResolutionPreset.max);
+    _cameraController = CameraController(_cameras[0], ResolutionPreset.high);
     _cameraController.initialize().then((_) {
      _isInitialized.value = true;
 
-     _cameraController.startImageStream((image) => print(DateTime.now().millisecondsSinceEpoch))
-     
+     _cameraController.startImageStream((image) => _cameraImage = image);
+
       
     }).catchError((Object e) {
       if (e is CameraException) {
@@ -41,6 +45,13 @@ class ScanController extends GetxController {
   }
 
   void capture() {
+    img.Image image = img.Image.fromBytes(_cameraImage.width,
+    _cameraImage.height,
+    _cameraImage.planes[0].bytes, format: img.Format.bgra);
+
+    Uint8List jpeg = Uint8List.fromList(img.encodeJpg(image));
+    print(jpeg.length);
+    print("Image Captured!");
 
   }
 }
