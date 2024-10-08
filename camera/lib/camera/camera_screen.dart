ddeb34fe8938c05bc63786372cf86b4dc44c1722
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 15:18:46 +0530

    Update camera_screen.dart

--- camera/lib/camera/camera_screen.dart
+++ camera/lib/camera/camera_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'camera_app/scan_controller.dart';
+
 
 class CameraScreen extends StatelessWidget {
   const CameraScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetX<ScanController>(builder: (controller) {
+      if (!controller.isInitialized) {
+        return Container();
+      }
+      return CameraPreview(controller.cameraController);
+    )
+    
   }
 }
