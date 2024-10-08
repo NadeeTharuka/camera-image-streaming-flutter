561f272d97f81032b89ff8b11ca02ee70f80665c
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 15:03:27 +0530

    Update scan_controller.dart

--- camera/lib/scan_controller.dart
+++ camera/lib/scan_controller.dart
@@ -1,3 +1,15 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/state_manager.dart';
 
-class ScanController extends GetxController {}
+class ScanController extends GetxController {
+  final RxBool _isInitialized = RxBool(false);
+
+  bool get isInitialized => _isInitialized.value;
+
+  Future<void> initCamera() {}
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+}
