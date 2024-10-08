f3a80abb546d64ac8f4326f22a271b4433fc2233
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-10-08 15:34:31 +0530

    Update camera_screen.dart

--- camera/lib/camera/camera_screen.dart
+++ camera/lib/camera/camera_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:camera_app/scan_controller.dart';
 import 'package:camera/camera.dart';
+import 'package:get/get.dart';
 
 class CameraScreen extends StatelessWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -13,9 +14,9 @@ class CameraScreen extends StatelessWidget {
         return Container();
       }
       return SizedBox(
-
-      child: CameraPreview(controller.cameraController));
-    )
-    
+          height: Get.height,
+          width: Get.width,
+          child: CameraPreview(controller.cameraController));
+    });
   }
 }
